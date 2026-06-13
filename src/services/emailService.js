e0a6aa6a46d0079872f47538dd44adeb4d6b4434
gulabdev01 <gulab@mdws.in>
2026-06-13 18:00:09 +0530

    fix email send

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "yourmail@gmail.com",
-    pass: "your_app_password",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
