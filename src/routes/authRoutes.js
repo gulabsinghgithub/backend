e56de26a917650db70cb76ffb2c38aac23734d24
gulabdev01 <gulab@mdws.in>
2026-06-13 17:11:56 +0530

    fix controller path

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controllers/authcontroller");
+// const { registerUser } = require("../controllers/authcontroller");
 
+// router.post("/register", registerUser);
+const {
+  registerUser,
+  sendOTP,
+  verifyOTP,
+} = require("../controllers/authcontroller");
+
+// Routes
 router.post("/register", registerUser);
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
 
 module.exports = router;
