c22eedeab2e32831a2b14762c700a4ea3ce403d6
gulabdev01 <gulab@mdws.in>
2026-06-13 17:08:43 +0530

    fix controller path

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  registerUser,
-  sendOTP,
-  verifyOTP,
-} = require("../controllers/authController");
+const { registerUser } = require("../controllers/authcontroller");
 
-// Async handler wrapper
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-console.log("Current file:", __filename);
-console.log("Current folder:", __dirname);
-router.post("/register", asyncHandler(registerUser));
-router.post("/send-otp", asyncHandler(sendOTP));
-router.post("/verify-otp", asyncHandler(verifyOTP));
+router.post("/register", registerUser);
 
 module.exports = router;
